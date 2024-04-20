8b949c1a8ae4883ae522eeb595023b9089ba9a90
adam-gill <adam.douglas.gill@gmail.com>
2024-04-19 21:13:09 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="flex items-center justify-center flex-col">
-        <h1 className="text-8xl mb-4">Daniel Ha 😍</h1>
+        <h1 className="text-8xl mb-4">Daniel Ha 😍😍</h1>
         <Image src="/daniel.png" alt="daniel" width={400} height={400}></Image>
       </div>
     </>
