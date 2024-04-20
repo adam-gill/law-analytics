197aa14e71268b6fcfb31fbe541bffb1d04bd44f
adam-gill <adam.douglas.gill@gmail.com>
2024-04-19 21:22:49 -0400

    add image

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Law Firm Embedded Analytics",
   description: "Web app for the BCAMP Law Firm Project",
+  openGraph: {
+    images: "https://bcampdev.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fd3eba391-ed29-4ab2-894d-a1d1a71754ad%2Ff1e596aa-96c4-4dc5-8a71-27b8675dcc9e%2FUntitled.png?table=block&id=bdb7385e-6265-4adb-baa2-975290f68a06&spaceId=d3eba391-ed29-4ab2-894d-a1d1a71754ad&width=800&userId=&cache=v2"
+  }
 };
 
 export default function RootLayout({
