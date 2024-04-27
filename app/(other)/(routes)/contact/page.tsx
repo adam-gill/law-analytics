82a7cc58d5840243f93112422088f2556f638c30
adam-gill <adam.douglas.gill@gmail.com>
2024-04-26 21:50:54 -0400

    add contact page so vercel would deploy

---
+++ app/(other)/(routes)/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface contactProps {
+    
+}
+
+const contact: React.FC<contactProps> = () => {
+    return (
+        <div>
+            Contact
+        </div>
+    );
+};
+
+export default contact;
