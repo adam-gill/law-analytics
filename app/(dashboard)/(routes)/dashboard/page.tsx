f3f2f284641fdc330b4d36f701fe525bdff32f1e
adam-gill <adam.douglas.gill@gmail.com>
2024-05-02 20:02:58 -0400

    fix repo

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -12,47 +12,90 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { auth } from "@/firebase/Init";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  function login() {
-    setLoggedIn(!loggedIn);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login(email: string , password: string) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setLoggedIn(true);
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+      })
   }
+
+  const notify = (msg: string) => toast(msg, {
+    position: "top-center",
+    autoClose: 3000,
+    theme: "dark"
+  })
 
   return (
     <>
       <div className="flex items-center justify-center flex-col gap-y-8">
-        <Card className="w-[400px] bg-[#192339] pb-6">
-          <CardHeader className="flex items-start justify-center">
-            <CardTitle className="text-white">Login</CardTitle>
-            <CardDescription>Login to View Your Dashboards</CardDescription>
-          </CardHeader>
-          <CardContent className="text-white pb-0">
-            <p className="text-lg">Email</p>
-          </CardContent>
-          <Input
-            className="bg-[#192339] items-start ml-6 w-[90%] text-white text-md mb-2 outline-2 border-slate-500 focus:border-white"
-            placeholder="example@email.com"
-          ></Input>
-          <CardContent className="text-white pb-0">
-            <p className="text-lg">Password</p>
-          </CardContent>
-          <Input
-            className="bg-[#192339] items-start ml-6 w-[90%] text-white mb-2 outline-1 border-slate-500 focus:border-white"
-            placeholder="password"
-            type="password"
-          ></Input>
-          <CardContent className="flex items-center justify-center pb-0">
-            <Button onClick={() => {login()}}className="bg-gradient-to-r from-purple-500 via-purple-400 to-blue-300 rounded-full items-center justify-center w-[200px] text-lg mt-2 hover:brightness-90">
-              Login
-            </Button>
-          </CardContent>
-        </Card>
+        {!loggedIn && (
+          <Card className="w-[400px] bg-[#192339] pb-6">
+            <CardHeader className="flex items-start justify-center">
+              <CardTitle className="text-white">Login</CardTitle>
+              <CardDescription>Login to View Your Dashboards</CardDescription>
+            </CardHeader>
+            <CardContent className="text-white pb-0">
+              <p className="text-lg">Email <span className="text-red-500">*</span></p>
+            </CardContent>
+            <Input
+              required
+              onChange={(event) => setEmail(event.target.value)}
+              className="bg-[#192339] items-start ml-6 w-[90%] text-white text-md mb-2 outline-2 border-slate-500 focus:border-white"
+              placeholder="example@email.com"
+            ></Input>
+            <CardContent className="text-white pb-0">
+              <p className="text-lg">Password <span className="text-red-500">*</span></p>
+            </CardContent>
+            <Input
+              required
+              onChange={(event) => setPassword(event.target.value)}
+              className="bg-[#192339] items-start ml-6 w-[90%] text-white mb-2 outline-1 border-slate-500 focus:border-white"
+              placeholder="password"
+              type="password"
+              onKeyDown={(e) => e.key === "Enter" && login(email, password)}
+            ></Input>
+            <CardContent className="flex items-center justify-center pb-0">
+              <Button
+                onClick={() => {
+                  if (email !== "" && password !== "") 
+                    login(email, password);
+                  else
+                    notify("⚠️ Email and password required.")
+                }}
+                className="bg-gradient-to-r from-purple-500 via-purple-400 to-blue-300 rounded-full items-center justify-center w-[200px] text-lg mt-2 hover:brightness-90"
+              >
+                Login
+              </Button>
+              <ToastContainer position="top-center" theme="dark"/>
+            </CardContent>
+          </Card>
+        )}
 
         {loggedIn ? (
           <iframe
-            className="w-[1000px] h-[600px]"
+            className="w-[1000px] h-[800px]"
             src="https://fenix-ai-partner.domo.com/embed/card/private/qYkzG"
           ></iframe>
         ) : (
@@ -64,3 +107,6 @@ const Dashboard: React.FC<DashboardProps> = () => {
 };
 
 export default Dashboard;
+
+
+// TODO : add error handling for login and loading states
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <div className="flex items-center justify-center flex-col">
-        <Image className="rounded-sm mt-4" src="/fenix.png" alt="daniel" width={400} height={400}></Image>
-        <h1 className="text-5xl max-w-[600px] text-center lh mt-4">Cutting-Edge Law Firm Embedded Analytics at Your Fingertips.</h1>
-        <Button className="bg-gradient-to-r from-purple-500 via-purple-400 to-blue-300 text-[#111827] text-2xl p-10 mt-12 rounded-full btn-hover">Get Started Now.</Button>
+        <Image
+          className="rounded-sm mt-4"
+          src="/fenix.png"
+          alt="daniel"
+          width={400}
+          height={400}
+        ></Image>
+        <h1 className="text-5xl max-w-[600px] text-center lh mt-4">
+          Cutting-Edge Law Firm Embedded Analytics at Your Fingertips.
+        </h1>
+        <Button
+          onClick={() => router.push("/dashboard")}
+          type="button"
+          className="bg-gradient-to-r from-purple-500 via-purple-400 to-blue-300 text-[#111827] text-2xl p-10 mt-12 rounded-full btn-hover"
+        >
+          Get Started Now.
+        </Button>
       </div>
     </>
   );
