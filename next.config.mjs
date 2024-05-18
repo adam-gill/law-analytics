0d91b1ccf53a71d0733270cc22b76083d579629a
adam-gill <adam.douglas.gill@gmail.com>
2024-05-18 18:58:19 -0400

    edited CSP header, hopefully it works in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+
+    frame-src https://fenix-ai-partner.domo.com https://dev-flrrksi5yy3p8j14.us.auth0.com https://embed.domo.com;
+    frame-ancestors 'self' https://fenix-ai-partner.domo.com 'self' https://dev-flrrksi5yy3p8j14.us.auth0.com https://embed.domo.com;
+`
+
+
+// trying to edit CSP to allow iframes here 
+// https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy#adding-a-nonce-with-middleware
+// https://stackoverflow.com/questions/63989613/how-to-allow-all-frame-ancestors-with-csp-header
+
 export default {
     reactStrictMode: true,
     // ... other configurations (if any)
@@ -9,15 +21,11 @@ export default {
           headers: [
             {
               key: 'Content-Security-Policy',
-              value: "frame-ancestors 'self' https://fenix-ai-partner.domo.com/",
+              value: cspHeader.replace(/\n/g, ''),
             },
-            {
-                key: 'Content-Security-Policy',
-                value: "frame-ancestors 'self' https://dev-flrrksi5yy3p8j14.us.auth0.com/",
-            }
           ],
         },
-      ];
+      ]
     },
   };
   
